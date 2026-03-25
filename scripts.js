a22b64801295ff89cf5fbef52e4ca1729f8d562b
I1HN <eloyfel@outlook.com>
2026-03-25 08:51:26 -0300

    Programação de subtração

--- scripts.js
+++ scripts.js
@@ -4,6 +4,6 @@ let resultadosub;
 
 numUmsub = Number(prompt("Escreva o seu primeiro número."));
 numDoissub = Number(prompt("Escreva o seu segundo número."));
-resultadosub = numUmsub - numDoissub
+resultadosub = numUmsub - numDoissub;
 
-alert(`O resultado da subtração é: ${result.toFixed}`)
+alert(`O resultado da subtração é: ${resultadosub.toFixed(2)}`);
