e1f3c2492fc94d2ac68cd5da02794ac8fa80714c
Anna Gabrielly <annagmartins08@gmail.com>
2026-03-25 08:30:07 -0300

    finalizando let

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 let num1soma;
 let num2soma;
 let resultado;
-
 let num1soma = Number(prompt("adicione o primeiro número"));
 let num2soma = Number(prompt("adicione o segundo número"));
 let resultado = num1soma + num1soma;
