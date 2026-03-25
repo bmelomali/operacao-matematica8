9051b5ecb4628635cf7c57498a842ec66d00fe3c
I1HN <gabrielgalbiati010@gmail.com>
2026-03-25 08:22:03 -0300

    Criei uma conta de divisão que utiliza apenas dois numeros

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-g
+let divisaoUm ;
+let divisaoDois;
+let result;
+
+divisaoUm = Number(prompt("Escreva um numero: "));
+divisaoDois = Number(prompt("escreva o sugundo numero: "));
+result = divisaoUm/divisaoDois
+
+alert(`Seu resultado é: ${result.toFixed(2)}`);
+
+
