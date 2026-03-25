b21f9d3a01e6ad1882ca4234733fe19b404a7aeb
I1HN <malikabertonha@gmail.com>
2026-03-25 09:35:45 -0300

    operação concluida

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-g
+let numMulti;
+let numMulti2;
+let resultado;
+
+numMulti = Number(prompt("adiciona o primeiro número"));
+numMulti2 = Number(prompt("adiciona o segundo número"));
+resultado = numMulti * numMulti2
+
+alert(`resultado da multiplicação é ${resultado}`);
