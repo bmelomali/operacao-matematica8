01842fdc7fe481e92319da987deb2d34b4f6239d
I1HN <eloyfel@outlook.com>
2026-03-25 08:55:35 -0300

    Programação de subtração

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 let numUmsub;
 let numDoissub;
 let resultadosub;
-
+// Let serve para criar a variavel
 numUmsub = Number(prompt("Escreva o seu primeiro número."));
 numDoissub = Number(prompt("Escreva o seu segundo número."));
 resultadosub = numUmsub - numDoissub;
-
+// Number e prompt faz a programação ver os números como numeros mesmo. Sem o texto.
 alert(`O resultado da subtração é: ${resultadosub.toFixed(2)}`);
+// Alert serve para aparecer a escrita na tela.
